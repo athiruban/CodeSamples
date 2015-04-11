e9ea3d695d3ff5a31402a49ba064ef0553c8abe6
Athiruban SM <ruban.athi@gmail.com>
2015-04-11 21:10:53 +0530

    solving pascal triangle

---
+++ showcase/athiruban/pascal_triangle_generation/Pascal.java
@@ -0,0 +1,16 @@
+
+
+/* Program to display the Pascal Triangle */
+
+public class Pascal{
+    public static void main(String args[]){
+    }
+
+    public Pascal(){
+    }
+
+    public display(int n){
+
+    }
+}
+
